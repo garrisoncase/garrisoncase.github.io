f83c5e3194d941467374b3dba021db0d63e7b14b
gbcase <gcasecollege@gmail.com>
2023-02-07 00:17:51 -0500

    Remove bogus images

--- js/app-aron.js
+++ js/app-aron.js
@@ -1,25 +1,16 @@
 "use strict"
 
 const container = document.querySelector(".container")
-const coffees = [
-  { name: "Perspiciatis", image: "images/coffee1.jpg" },
-  { name: "Voluptatem", image: "images/coffee2.jpg" },
-  { name: "Explicabo", image: "images/coffee3.jpg" },
-  { name: "Rchitecto", image: "images/coffee4.jpg" },
-  { name: " Beatae", image: "images/coffee5.jpg" },
-  { name: " Vitae", image: "images/coffee6.jpg" },
-  { name: "Inventore", image: "images/coffee7.jpg" },
-  { name: "Veritatis", image: "images/coffee8.jpg" },
-  { name: "Accusantium", image: "images/coffee9.jpg" },
+const attributes = [
+  { name: "Hitpoints: " }
 ]
 
-const showCoffees = () => {
+const showAttributes = () => {
     let output = ""
-    coffees.forEach(
-      ({ name, image }) =>
+    attributes.forEach(
+      ({ name }) =>
         (output += `
                 <div class="card">
-                  <img class="card--avatar" src=${image} />
                   <h1 class="card--title">${name}</h1>
                   <a class="card--link" href="#">Taste</a>
                 </div>
@@ -28,7 +19,7 @@ const showCoffees = () => {
     container.innerHTML = output
   }
   
-document.addEventListener("DOMContentLoaded", showCoffees)
+document.addEventListener("DOMContentLoaded", showAttributes)
 
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
