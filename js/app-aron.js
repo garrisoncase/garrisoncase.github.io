6279b57bc2cf544c52cd237e5b340d2627a7c649
gbcase <gcasecollege@gmail.com>
2023-02-07 00:47:18 -0500

    manifest linking

--- js/app-aron.js
+++ js/app-aron.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", showAttributes)
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
     navigator.serviceWorker
-        .register("/serviceWorker.js")
+        .register("js/serviceWorker.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
